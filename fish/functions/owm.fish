7ca94dc81109ecfb25209e174ce2dbbab0e534a8
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2016-05-27 11:47:10 +0200

    fix units parameter in owm function

--- fish/functions/owm.fish
+++ fish/functions/owm.fish
@@ -1,3 +1,3 @@
 function owm
-  command curl -s "http://pro.openweathermap.org/data/2.5/group?APPID=$OWM_APP_ID&unit=metric&id=$argv"
+  command curl -s "http://pro.openweathermap.org/data/2.5/group?APPID=$OWM_APP_ID&units=metric&id=$argv"
 end
