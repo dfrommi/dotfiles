41dfee2205f18ded658136a7bf73032450c19fa4
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2016-09-07 10:08:47 +0200

    use single weather owm api instead of group

--- fish/functions/owm.fish
+++ fish/functions/owm.fish
@@ -1,3 +1,3 @@
 function owm
-  command curl -s "http://pro.openweathermap.org/data/2.5/group?APPID=$OWM_APP_ID&units=metric&id=$argv"
+  command curl -s "http://pro.openweathermap.org/data/2.5/weather?APPID=$OWM_APP_ID&units=metric&id=$argv"
 end
