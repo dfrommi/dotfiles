b097cf56f076dc45e389617b2c8fa6cd62ceef4b
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2017-07-10 22:46:51 +0200

    misc improvements and bundle integrations in fish

--- fish/config.fish
+++ fish/config.fish
@@ -11,18 +11,32 @@ set -x DEFAULT_USER dennis
 set -x GROOVY_HOME '/usr/local/opt/groovy/libexec'
 set -x JAVA_HOME (/usr/libexec/java_home)
 
-set -x PATH ~/my/bin /Applications/Docker.app/Contents/Resources/bin $PATH
+set -x PATH ~/my/bin /Applications/Docker.app/Contents/Resources/bin $HOME/.sdkman/candidates/*/current/bin $PATH
+
+set -x FZF_FIND_FILE_COMMAND "ag -l -g '' --ignore .git --ignore Library"
+
+set -x EDITOR micro
+set -x MICRO_TRUECOLOR 1
 
 abbr brwe brew
 abbr cask brew cask
 
-alias g git
+abbr g git
 abbr gcm git commit -m
+abbr gca git commit --amend --no-edit
+
+alias e micro
+alias cat ccat
 
 # Keep Fisherman out of regular fish config, otherwise all symlinks have to be added to gitignore
+# fisher tool assets, like fisher completion
 set -g fish_config $HOME/.config/fisherman
+# symbolic links to functions etc.
 set -g fish_path $HOME/.config/fisherman
+# installed packages
+set -g fisher_config $HOME/.config/fisherman
 set -g fisher_file $HOME/.config/fish/fishfile
+
 set fish_function_path $fish_config/functions $fish_function_path
 set fish_complete_path $fish_config/completions $fish_complete_path
 # install missing packages
@@ -30,3 +44,8 @@ fisher -q
 
 test -e ~/.config/fish/config.local.fish ; and source ~/.config/fish/config.local.fish
 test -e ~/.config/fish/functions/iterm2_print_user_vars.fish ; and source ~/.config/fish/functions/iterm2_print_user_vars.fish
+
+# Install event handlers
+__git_status
+__touchbar_git_root_display
+__touchbar_git_branch_display
