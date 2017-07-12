312e3482e280345368492809f45ee6433db06357
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2017-07-12 20:37:24 +0200

    split config.fish in completions and conf.d files

---
+++ fish/conf.d/abbr_and_alias.fish
@@ -0,0 +1,9 @@
+abbr brwe brew
+abbr cask brew cask
+
+abbr g git
+abbr gcm git commit -m
+abbr gca git commit --amend --no-edit
+
+alias e micro
+alias cat ccat
---
+++ fish/conf.d/fisherman.fish
@@ -0,0 +1,19 @@
+# Keep Fisherman out of regular fish config, otherwise all symlinks have to be added to gitignore
+# fisher tool assets, like fisher completion
+set -g fish_config $HOME/.config/fisherman
+# symbolic links to functions etc.
+set -g fish_path $HOME/.config/fisherman
+# installed packages
+set -g fisher_config $HOME/.config/fisherman
+set -g fisher_file $HOME/.config/fish/fishfile
+
+set fish_function_path $fish_config/functions $fish_function_path
+set fish_complete_path $fish_config/completions $fish_complete_path
+
+# install missing packages
+fisher -q
+
+# Fisherman conf.d is not detected
+for file in $fish_config/conf.d/*.fish
+  source $file
+end
