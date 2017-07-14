bcdece8447743bd945255a9754ebe45eb6d8a8bc
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2017-07-14 22:39:52 +0200

    move fisherman dir out of repo

--- fish/conf.d/fisherman.fish
+++ fish/conf.d/fisherman.fish
@@ -1,17 +1,17 @@
 # Keep Fisherman out of regular fish config, otherwise all symlinks have to be added to gitignore
 # fisher tool assets, like fisher completion
-set -g fish_config $HOME/.config/fisherman
+set -g fish_config $HOME/.fisherman
 # symbolic links to functions etc.
-set -g fish_path $HOME/.config/fisherman
+set -g fish_path $HOME/.fisherman
 # installed packages
-set -g fisher_config $HOME/.config/fisherman
+set -g fisher_config $HOME/.fisherman
 set -g fisher_file $HOME/.config/fish/fishfile
 
 set fish_function_path $fish_config/functions $fish_function_path
 set fish_complete_path $fish_config/completions $fish_complete_path
 
 # install missing packages
-fisher -q
+#fisher -q
 
 # Fisherman conf.d is not detected
 for file in $fish_config/conf.d/*.fish
