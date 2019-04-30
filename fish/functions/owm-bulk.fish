dad0e049f4bf48555aad3e44dc9e400753e8891d
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2019-05-01 00:56:53 +0200

    remove OWM functions

--- fish/functions/owm-bulk.fish
+++
@@ -1,3 +0,0 @@
-function owm-bulk
-  command curl -s "http://pro.openweathermap.org/data/2.5/group?APPID=$OWM_APP_ID&units=metric&id=$argv"
-end
--- fish/functions/owm.fish
+++
@@ -1,3 +0,0 @@
-function owm
-  command curl -s "http://pro.openweathermap.org/data/2.5/weather?APPID=$OWM_APP_ID&units=metric&id=$argv"
-end
