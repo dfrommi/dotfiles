f2e5c484ee26c17697036069f5c61474060de8a5
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2021-05-02 21:13:10 +0200

    remove unused fish environment variables after leaving tado

--- fish/conf.d/config.fish
+++ fish/conf.d/config.fish
@@ -2,10 +2,9 @@ set -g -x fish_greeting ''
 
 set -x DEFAULT_USER dennis
 
-set -x GROOVY_HOME '/usr/local/opt/groovy/libexec'
 set -x JAVA_HOME (/usr/libexec/java_home)
 
-set -x PATH ~/my/bin ~/thirdparty/bin /Applications/Docker.app/Contents/Resources/bin $HOME/.sdkman/candidates/*/current/bin $PATH
+set -x PATH ~/my/bin ~/thirdparty/bin /Applications/Docker.app/Contents/Resources/bin $PATH
 
 set -x FZF_LEGACY_KEYBINDINGS 0
 set -x FZF_FIND_FILE_COMMAND "ag -l -g '' --ignore .git --ignore Library"
@@ -13,6 +12,4 @@ set -x FZF_FIND_FILE_COMMAND "ag -l -g '' --ignore .git --ignore Library"
 set -x EDITOR micro
 set -x MICRO_TRUECOLOR 1
 
-set -x GIT_REPOS_BASE_PATHS $HOME/{my,thirdparty,thirdparty/openhab2/git,tado/repo} $HOME
-
-#nvm use --silent default
+set -x GIT_REPOS_BASE_PATHS $HOME/{my,thirdparty} $HOME
