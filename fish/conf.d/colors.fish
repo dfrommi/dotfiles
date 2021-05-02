17d1879193d72ecee97d9f5e6149bdfdef5413b1
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2021-05-02 21:12:05 +0200

    move custom color setup from fish_variables to dedicated init script

--- fish/conf.d/colors.fish
+++ fish/conf.d/colors.fish
@@ -1,1 +1,9 @@
 set fish_color_error brmagenta
+set fish_color_command --bold
+set fish_color_comment blue
+set fish_color_end brmagenta
+set fish_color_escape bryellow --bold
+set fish_color_operator bryellow
+set fish_color_param cyan 
+set fish_color_quote yellow
+set fish_color_redirection brblue 
