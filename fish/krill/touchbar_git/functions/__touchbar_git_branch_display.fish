08c668e11060171631017b8f538b04decf056ba2
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2022-08-28 10:22:25 +0200

    add ticket-support to git touchbar

--- fish/krill/touchbar_git/functions/__touchbar_git_branch_display.fish
+++ fish/krill/touchbar_git/functions/__touchbar_git_branch_display.fish
@@ -1,6 +1,22 @@
-function __touchbar_git_branch_display --on-variable GIT_BRANCH
+function __touchbar_git_branch_display --on-variable GIT_BRANCH --on-variable JIRA_TICKET_ID
   if [ "$GIT_BRANCH" ]
-    echo -ne "\033]1337;SetKeyLabel=F3=⌥ $GIT_BRANCH\a"
+    switch "$GIT_BRANCH"
+      case "feature/*"
+        set icon "🤓"
+        set branch (string replace 'feature/' '' $GIT_BRANCH)
+      case "bugfix/*"
+        set icon "🐞"
+        set branch (string replace 'bugfix/' '' $GIT_BRANCH)
+      case "*"
+        set icon "⌥"
+        set branch $GIT_BRANCH
+    end
+
+    if [ "$JIRA_TICKET_ID" ]
+      set branch (string replace "$JIRA_TICKET_ID-" "" $branch)
+    end
+
+    echo -ne "\033]1337;SetKeyLabel=F3=$icon $branch\a"
   else
     echo -ne "\033]1337;SetKeyLabel=F3=F3\a"
   end
--- fish/krill/touchbar_git/functions/__touchbar_git_ticket_display.fish
+++ fish/krill/touchbar_git/functions/__touchbar_git_ticket_display.fish
@@ -1,6 +1,6 @@
 function __touchbar_git_ticket_display --on-variable JIRA_TICKET_ID
   if [ "$JIRA_TICKET_ID" ]
-    echo -ne "\033]1337;SetKeyLabel=F4=🐞 $JIRA_TICKET_ID\a"
+    echo -ne "\033]1337;SetKeyLabel=F4=📎 $JIRA_TICKET_ID\a"
   else
     echo -ne "\033]1337;SetKeyLabel=F4=F4\a"
   end
