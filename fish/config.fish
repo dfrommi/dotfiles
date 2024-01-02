f076a19d6087a18f7097b2766bd92350c2491131
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2024-01-02 13:56:15 +0100

    Configure conda in fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,11 @@ end
 if status is-interactive
   starship init fish | source
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
