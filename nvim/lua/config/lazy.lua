9594bdd6c95d0827eeb20091cde2376b3f7322bc
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2024-01-09 20:23:58 +0100

    Use LazyVim to configure NeoVim

---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,43 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
+
+require("lazy").setup({
+  spec = {
+    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    -- not working { import = "lazyvim.plugins.extras.lang.json" },
+    -- not working { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
+    { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "plugins" },
+  },
+  defaults = {
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  -- install = {
+  --   colorscheme = { "catppuccin-mocha" },
+  -- },
+  checker = { enabled = true }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
---
+++ nvim/lua/plugins/lazy.lua
@@ -0,0 +1,11 @@
+return {
+  "LazyVim/LazyVim",
+  opts = {
+    colorscheme = "catppuccin-mocha",
+    defaults = {
+      --autocmds = true, -- lazyvim.config.autocmds
+      keymaps = false, -- lazyvim.config.keymaps
+      --options = true, -- lazyvim.config.options
+    },
+  },
+}
