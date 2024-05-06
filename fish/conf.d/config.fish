0ce32e514c6aeed43e4eec6753bb9a8ec7de43c3
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2024-05-06 10:22:30 +0200

    Configure asdf and rust in fish

--- fish/conf.d/config.fish
+++ fish/conf.d/config.fish
@@ -2,14 +2,11 @@ set -g -x fish_greeting ''
 
 set -x DEFAULT_USER dennis
 
-set -x JAVA_HOME (/usr/libexec/java_home)
-
-set -x PATH ~/my/bin ~/thirdparty/bin $PATH /Applications/Docker.app/Contents/Resources/bin
+set -x PATH ~/my/bin ~/thirdparty/bin $PATH
 
 set -x FZF_LEGACY_KEYBINDINGS 0
 set -x FZF_FIND_FILE_COMMAND "ag -l -g '' --ignore .git --ignore Library"
 
-set -x MICRO_TRUECOLOR 1
 set -x BAT_THEME Coldark-Dark
 
 set -x GIT_REPOS_BASE_PATHS $HOME/{my,thirdparty} $HOME
