4a166fe51718133bc268ad6b1da650248c527be3
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2024-09-25 21:03:25 +0200

    Configure aichat cli tool

---
+++ fish/conf.d/aichat.fish
@@ -0,0 +1,10 @@
+function _aichat_fish
+    set -l _old (commandline)
+    if test -n $_old
+        echo -n "⌛"
+        commandline -f repaint
+        commandline (aichat -e $_old)
+    end
+end
+
+bind \ei _aichat_fish
