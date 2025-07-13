de270f3c205bd3b26d5624caf21d5f3d486aaa0c
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2025-07-13 12:13:10 +0200

    Consolidate all AI plugins into a single lua file

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -29,7 +29,9 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.ai.supermaven" },
+    -- { import = "lazyvim.plugins.extras.ai.supermaven" },
+    { import = "lazyvim.plugins.extras.ai.copilot" },
+    -- { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "plugins" },
   },
   defaults = {
