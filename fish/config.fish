5ccce9f12d176b8fb7fa94b9bb7d65ec76118704
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2026-04-07 20:46:14 +0200

    [fish] Add mason-bin to path

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ eval (/opt/homebrew/bin/brew shellenv)
 
 #add shims manually after homebrew to make sure they are at the front of the path
 fish_add_path -m $HOME/.asdf/shims
+fish_add_path -m $HOME/.local/share/nvim/mason/bin
 
 if status is-interactive
     starship init fish | source
