f7f7b4c8d2ac4b96834773a3c6aa5071c0ae0d32
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2026-09-09 12:54:20 +0200

    [fish] update directory keybindings to be more vi-like

--- fish/krill/fzf/conf.d/fzf_init.fish
+++ fish/krill/fzf/conf.d/fzf_init.fish
@@ -1,9 +1,9 @@
 abbr -a !d --position anywhere -f fzf_directory_cwd
-abbr -a !dg --position anywhere -f fzf_directory_git_root
+abbr -a !dr --position anywhere -f fzf_directory_git_root
 abbr -a !dh --position anywhere -f fzf_directory_home
 
 abbr -a !f --position anywhere -f fzf_file_cwd
-abbr -a !fg --position anywhere -f fzf_file_git_root
+abbr -a !fr --position anywhere -f fzf_file_git_root
 
 abbr -a !z --position anywhere -f fzf_zoxide
 
